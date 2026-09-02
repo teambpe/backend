0d1843e790afc1990e5cf4a0b7194b07c10c5553
teambpe <teambpe2000@gmail.com>
2026-09-02 15:54:03 +0530

    Update db.js

--- db.js
+++ db.js
@@ -1,26 +1,20 @@
 const mysql = require('mysql2');
-const fs = require('fs');
-const path = require('path');
 require('dotenv').config();
 
 const db = mysql.createConnection({
   host: process.env.DB_HOST,
-  port: Number(process.env.DB_PORT) ,
-
+  port: Number(process.env.DB_PORT),
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-   ssl: {
-     ca: fs.readFileSync(path.join(__dirname, 'certs/ca.pem')),
-    rejectUnauthorized: false // or false for testing, but true is safer
-  }
+  database: process.env.DB_NAME
 });
 
 db.connect((err) => {
   if (err) {
     console.error('❌ MySQL connection failed:', err);
-    throw err;
+    return;
   }
+
   console.log('✅ MySQL connected');
 });
 
