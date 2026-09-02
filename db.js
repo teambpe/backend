eba5554b62b0947f2e37e3ba3da7ca890874037e
teambpe <teambpe2000@gmail.com>
2026-09-02 16:02:19 +0530

    Update db.js

--- db.js
+++ db.js
@@ -1,6 +1,13 @@
 const mysql = require('mysql2');
 require('dotenv').config();
 
+console.log('🔍 MySQL Config:', {
+  host: process.env.MYSQL_HOST,
+  port: process.env.MYSQL_PORT,
+  user: process.env.MYSQL_USER,
+  database: process.env.MYSQL_DATABASE
+});
+
 const db = mysql.createConnection({
   host: process.env.MYSQL_HOST,
   port: Number(process.env.MYSQL_PORT),
