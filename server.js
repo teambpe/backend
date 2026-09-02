dd9dca2f59a6b387a8ee3ef7edad80a7c061d84d
teambpe <teambpe2000@gmail.com>
2026-09-02 15:40:28 +0530

    Update server.js

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const authRoutes = require('./routes/auth');
 const visitorRoutes = require('./routes/visitors');
 const db = require('./db'); 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 //const multer = require('multer');
 //const path = require('path');
 
